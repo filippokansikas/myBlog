8732db9e452ffcecc763c0e92d89dbc27b3685e0
“Filippo <filippo.kansikas@refresh-academy.org>
2025-02-07 09:24:51 +0100

    version-23

--- crypto.js
+++ crypto.js
@@ -6,7 +6,7 @@ var doge = document.getElementById("dogecoin");
 var liveprice = {
     "async": true,
     "scroosDomain": true,
-    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cxrp%2Cethereum%2Cdogecoin&vs_currencies=usd",
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Clitecoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
 
     "method": "GET",
     "headers": {}
